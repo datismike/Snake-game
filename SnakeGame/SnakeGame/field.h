5bdd644668c07d6944d888bf8e19ce907446b331
Michael Baranovski <datismike@gmail.com>
2019-10-21 19:04:08 +0300

    Field size has been changed

--- SnakeGame/SnakeGame/field.h
+++ SnakeGame/SnakeGame/field.h
@@ -1,8 +1,12 @@
 #pragma once
-#define CELL_WIDTH 15
-#define CELL_HEIGHT 15
-#define FIELD_WIDTH 20
-#define FIELD_HEIGHT 20
-#define BACKGROUND_COLOR 0x00E0E0E0
+#define CELL_WIDTH 20
+#define CELL_HEIGHT 20
+#define FIELD_WIDTH 21
+#define FIELD_HEIGHT 22
+#define BACKGROUND_COLOR 0x00CCE5FF
+#define CELL_COLOR 0x00000000
+#define CELL_MARGIN 1
 #define WINDOW_CLASS_NAME "MainWindowClass"
 #define WINDOW_NAME "Sanke Game"
+#define TIMER_TICK_ID 1
+#define TIMER_TICK_ELAPSE 500
